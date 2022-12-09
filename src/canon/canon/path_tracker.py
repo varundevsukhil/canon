42f561a4d6b2fc72a81ba677fb724003dab4ef10
Varundev Sukhil <vsukhil@gmail.com>
2022-12-08 22:01:20 -0800

    added lateral control; needs testing

--- src/canon/canon/path_tracker.py
+++ src/canon/canon/path_tracker.py
@@ -47,6 +47,15 @@ class PathTracker(Node):
         self.known_loc_idx = (-1, -1, -1)
         self.dyn_range_loc = (100, 200, 200)
 
+        # lateral control parameters
+        steer_max = 2.0
+        angle_max = 20.0
+        gradient = 0.005
+        candidates = [candidate * gradient for candidate in range(int(steer_max / gradient))]
+        negatives = [-candidate for candidate in candidates[1:]]
+        self.candidates = [math.radians(candidate) for candidate in negatives[::-1] + candidates]
+        self.steer_damping_factor = max(self.candidates) / angle_max / steer_max
+
         # path tracker parameters
         self.wheelbase_len = 3.0
         self.time_tick = 0.25
@@ -55,11 +64,11 @@ class PathTracker(Node):
         self.max_speed = 40.0
 
         # node publishers
-        self._lat_err_pub = self.create_publisher(Float64, f"/{racecar_ns}/path_tracker/lat_err", 1)
-        self._lon_err_pub = self.create_publisher(Float64, f"/{racecar_ns}/path_tracker/lon_err", 1)
-        self._inner_bounds_pub = self.create_publisher(PoseArray, f"/{racecar_ns}/path_server/inner_bounds", 1)
-        self._outer_bounds_pub = self.create_publisher(PoseArray, f"/{racecar_ns}/path_server/outer_bounds", 1)
-        self._target_ref_pub = self.create_publisher(PoseArray, f"/{racecar_ns}/path_server/target_spline", 1)
+        self.lat_err_pub = self.create_publisher(Float64, f"/{racecar_ns}/path_tracker/lat_err", 1)
+        self.lon_err_pub = self.create_publisher(Float64, f"/{racecar_ns}/path_tracker/lon_err", 1)
+        self.inner_bounds_pub = self.create_publisher(PoseArray, f"/{racecar_ns}/path_server/inner_bounds", 1)
+        self.outer_bounds_pub = self.create_publisher(PoseArray, f"/{racecar_ns}/path_server/outer_bounds", 1)
+        self.target_ref_pub = self.create_publisher(PoseArray, f"/{racecar_ns}/path_server/target_spline", 1)
 
         # node spinners and callbacks
         self.create_subscription(Odometry, f"/{racecar_ns}/odometry", self.path_tracker_node, 1)
@@ -72,11 +81,13 @@ class PathTracker(Node):
         self.tf.transform = Transform(translation = pos, rotation = rot)
         self.tf.header.stamp = self.get_clock().now().to_msg()
 
-    def yaw_to_quat(self, yaw: float) -> Quaternion:
-            quat   = Quaternion()
-            quat.z = math.sin(yaw / 2.0)
-            quat.w = math.cos(yaw / 2.0)
-            return(quat)
+    def yaw_to_quaternion(self, yaw: float) -> Quaternion:
+
+        # convert yaw to quaternion; ignore pitch and roll
+        quat   = Quaternion()
+        quat.z = math.sin(yaw / 2.0)
+        quat.w = math.cos(yaw / 2.0)
+        return(quat)
 
     def read_splines_info(self, trajectory: str) -> Tuple[PoseArray, float]:
 
@@ -92,7 +103,7 @@ class PathTracker(Node):
             pose = Pose()
             _next = (i + 1) % len(raw_pts)
             pose.position.x, pose.position.y = raw_pts[i][0], raw_pts[i][1]
-            pose.orientation = self.yaw_to_quat(math.atan2(raw_pts[_next][1] - raw_pts[i][1], raw_pts[_next][0] - raw_pts[i][0]))
+            pose.orientation = self.yaw_to_quaternion(math.atan2(raw_pts[_next][1] - raw_pts[i][1], raw_pts[_next][0] - raw_pts[i][0]))
             points.poses.append(pose)
         
         # calculate the average spline resolution
@@ -135,7 +146,7 @@ class PathTracker(Node):
         # return the localized spline index and the lateral seperation to the target spline
         return(_target_spline_loc_idx, _target_spline_lat_sep)
 
-    def localize_raccecar(self, state: Odometry) -> Tuple[Tuple[int], Tuple[float]]:
+    def localize_racecar(self, state: Odometry) -> Tuple[Tuple[int], Tuple[float]]:
 
         # localize the racecar for the active raceline and the track bounds
         _raceline_loc_idx, _raceline_lat_sep = self.localize_racecar_on_spline(state, int(SplineIdentifier.ActiveRaceline), self.first_loc_found)
@@ -176,7 +187,8 @@ class PathTracker(Node):
     def assemble_references(self, state: Odometry) -> Tuple[PoseArray, PoseArray, PoseArray]:
 
         # calculate the current velocity and enforce bounds vioaltion
-        vel = min(max(math.hypot(state.twist.twist.linear.x, state.twist.twist.linear.y), self.min_speed), self.max_speed)
+        vel = math.hypot(state.twist.twist.linear.x, state.twist.twist.linear.y)
+        vel = min(max(vel, self.min_speed), self.max_speed)
 
         # assemble reference spline pose array for the active raceline and the track bounds
         _raceline_ref = self.assemble_reference_points(vel, int(SplineIdentifier.ActiveRaceline), False)
@@ -184,19 +196,81 @@ class PathTracker(Node):
         _outer_bounds_ref = self.assemble_reference_points(vel, int(SplineIdentifier.TrackOuterBounds), True)
         return(_raceline_ref, _inner_bounds_ref, _outer_bounds_ref)
 
+    def yaw_from_quaternion(self, quat: Quaternion) -> float:
+
+        # extract yaw from quaternion
+        x = 1.0 - 2.0 * (math.pow(quat.y, 2) + math.pow(quat.z, 2))
+        y = 2.0 * (quat.w * quat.z + quat.x * quat.y)
+        yaw = math.atan2(y, x)
+        return(yaw)
+    
+    def iterate_current_cycle(self, x: float, y: float, yaw: float, vel: float, steer: float) -> Tuple[float, float, float]:
+
+        # predict the immediate next cycle using a simple kinematic bicycle model of the racecar
+        x += math.cos(yaw) * vel * self.time_tick
+        y += math.sin(yaw) * vel * self.time_tick
+        yaw += math.tan(steer) * vel / self.wheelbase_len * self.time_tick
+        return(x, y, yaw)
+    
+    def predict_motion_for_candidate(self, state: Odometry, candidate: float) -> PoseArray:
+
+        # decompose state vector
+        x, y = state.pose.pose.position.x, state.pose.pose.position.y
+        yaw = self.yaw_from_quaternion(state.pose.pose.orientation)
+        vel = math.hypot(state.twist.twist.linear.x, state.twist.twist.linear.y)
+        vel = min(max(vel, self.min_speed), self.max_speed)
+
+        # predict the motion for the entire horizon
+        prediction = PoseArray()
+        prediction.header.frame_id = "map"
+        for i in range(self.horizon_len):
+            pose = Pose()
+            x, y, yaw = self.iterate_current_cycle(x, y, yaw, vel, candidate)
+            pose.position.x, pose.position.y = x, y
+            pose.orientation = self.yaw_to_quaternion(yaw)
+            prediction.poses.append(pose)
+        return(prediction)
+    
+    def estimate_maneuver_cost(self, prediction: PoseArray, reference: PoseArray, _ref_lat_err: float) -> float:
+
+        # add lateral seperation between each prediction and reference
+        # use a inverse cubic cost decay model as pred distance increases
+        # specifically target to prioritize smooth convergence based on lateral seperation to the raceline
+        cost = 0.0
+        for i in range(len(reference.poses)):
+            cost_i = math.hypot(prediction.poses[i].position.x - reference.poses[i].position.x, prediction.poses[i].position.y - reference.poses[i].position.y)
+            target_i = min(max(1, int(_ref_lat_err)), self.horizon_len)
+            target_i = abs(target_i - i) + 1
+            cost += cost_i / math.pow(target_i, 3)
+        return(cost)
+    
+    def calculate_optimal_steer_candidate(self, state: Odometry, reference: PoseArray, _ref_lat_err: float) -> float:
+
+        # gather predictions for each candidate in the steering candidates
+        # compute the maneuver cost for each of the steering candidates
+        # the minimum cost index corresponds to the optimal steering candidate
+        predictions = [self.predict_motion_for_candidate(state, candidate) for candidate in self.candidates]
+        candidates_costs = [self.estimate_maneuver_cost(prediction, reference, _ref_lat_err) for prediction in predictions]
+        optimal_candidate = self.candidates[candidates_costs.index(min(candidates_costs))]
+        
+        # apply the output adjusted for steering damping ration
+        optimal_candidate = -1.0 * optimal_candidate / self.steer_damping_factor
+        return(optimal_candidate)
+
     def path_tracker_node(self, state: Odometry) -> None:
 
         # execute path tracker node tasks sequentially
         self.assemble_dynamic_tf(state)
-        self.known_loc_idx, _lat_err = self.localize_raccecar(state)
-        _refs = self.assemble_references(state)
+        self.known_loc_idx, lat_err = self.localize_racecar(state)
+        refs = self.assemble_references(state)
+        command_steer = self.calculate_optimal_steer_candidate(state, refs[int(SplineIdentifier.ActiveRaceline)], lat_err[int(SplineIdentifier.ActiveRaceline)])
 
         # pubish all the desired information
         self.tf_pub.sendTransform(self.tf)
-        self._lat_err_pub.publish(Float64(data = _lat_err[int(SplineIdentifier.ActiveRaceline)]))
-        self._inner_bounds_pub.publish(_refs[int(SplineIdentifier.TrackInnerBounds)])
-        self._outer_bounds_pub.publish(_refs[int(SplineIdentifier.TrackOuterBounds)])
-        self._target_ref_pub.publish(_refs[int(SplineIdentifier.ActiveRaceline)])
+        self.lat_err_pub.publish(Float64(data = lat_err[int(SplineIdentifier.ActiveRaceline)]))
+        self.inner_bounds_pub.publish(refs[int(SplineIdentifier.TrackInnerBounds)])
+        self.outer_bounds_pub.publish(refs[int(SplineIdentifier.TrackOuterBounds)])
+        self.target_ref_pub.publish(refs[int(SplineIdentifier.ActiveRaceline)])
 
 def path_tracker():
     rclpy.init()
